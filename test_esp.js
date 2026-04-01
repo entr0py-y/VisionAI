175e97885ed641b1ce2a6e7938806e26ae1457f1
Eren Yeager <erenyeager@Erens-MacBook-Air.local>
2026-04-01 10:50:00 +0530

    Cleanup repository by removing legacy test scripts and logs

--- test_esp.js
+++
@@ -1,9 +0,0 @@
-const { WaveFile } = require('wavefile');
-const rawData = Buffer.alloc(1024); // 1024 bytes of silence
-const base64Audio = rawData.toString('base64');
-
-const pcmBuffer = Buffer.from(base64Audio, 'base64');
-const wav = new WaveFile();
-const int16Array = new Int16Array(pcmBuffer.buffer, pcmBuffer.byteOffset, pcmBuffer.length / 2);
-wav.fromScratch(1, 16000, '16', int16Array);
-console.log('WAV Buffer size:', wav.toBuffer().length);
